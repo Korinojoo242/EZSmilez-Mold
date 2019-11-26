4e79c87069ccd899b36d5169ab85e21d0a8b3af9
Gabrielle Hollaender <hollaender.gabrielle@gmail.com>
2019-11-25 19:49:23 -0500

    Almost Done Step 1
    Scoop is still funky, but everything you need should be there 0_0

---
+++ EZ Smile Tooth Molds/Assets/Scripts/Collisions/SpoonCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpoonCollision : MonoBehaviour
+{
+    public GameObject scoop;
+    public GameObject myPrefab;
+   
+        void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "alginate")
+        {
+            Destroy(scoop);
+            Instantiate(myPrefab, new Vector3(0.9f, -3.5f, 1.48f), Quaternion.identity);
+        }
+    }
+}
