9c3b79659e43fc58b0e9a6394c526f24e67f0df0
Egor Lukoyanov <egor.lukoyanov@ontariotech.com>
2019-12-01 17:14:03 -0500

    Scene2Script

---
+++ EZ Smile Tooth Molds/Assets/Scripts/Sc.2Scripts/FullSpatulaToTray.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatulaToFull : MonoBehaviour
+{
+    public GameObject FullSpatula;
+    public GameObject FullTray1Prefab;
+    public GameObject FullTray1Prefab;
+    Vector3 spatulaPosition;
+    Vector3 ogPosition;
+    Vector3 Tray1Position;
+    Vector3 Tray1Position;
+
+    private void Start()
+    {
+        //ogPosition = scoop.transform.position;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "tary1")
+        {
+            spatulaPosition = spatula.transform.position - new Vector3(0, 0, 2);
+            Destroy(spatula);
+            Instantiate(FullSpatulaPrefab, spatulaPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ EZ Smile Tooth Molds/Assets/Scripts/Sc.2Scripts/SpatulaToFull.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatulaToFull : MonoBehaviour
+{
+    public GameObject spatula;
+    public GameObject FullSpatulaPrefab;
+    Vector3 spatulaPosition;
+    Vector3 ogPosition;
+
+    private void Start()
+    {
+        //ogPosition = scoop.transform.position;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "mixture")
+        {
+            spatulaPosition = spatula.transform.position - new Vector3(0, 0, 2);
+            Destroy(spatula);
+            Instantiate(FullSpatulaPrefab, spatulaPosition, Quaternion.identity);
+        }
+    }
+}
