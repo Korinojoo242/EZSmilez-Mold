b91fd51f28849ef1a742401bc334bbc56eda898e
Gabrielle Hollaender <hollaender.gabrielle@gmail.com>
2019-12-01 14:25:44 -0500

    Step 1 Mostly Functioning

--- EZ Smile Tooth Molds/Assets/Scripts/Collisions/BottleCollision.cs
+++ EZ Smile Tooth Molds/Assets/Scripts/Collisions/BottleCollision.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class BottleCollision : MonoBehaviour 
 {
-    public GameObject bottle;
-    void OnCollisionEnter(Collision collision) 
-    { 
-        if (collision.gameObject.name == "Bowl") 
-        { 
-            Destroy(bottle); 
-        } 
-    } 
+    Vector3 originalPos;
+    public GameObject Bottle;
+
+    void Start()
+    {
+        originalPos = gameObject.transform.position;
+        //alternatively, just: originalPos = gameObject.transform.position;
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bowl")
+        {
+            Destroy(Bottle);
+        }
+
+        else
+        {
+            gameObject.transform.position = originalPos;
+        }
+    }
 }
--- EZ Smile Tooth Molds/Assets/Scripts/Collisions/SpoonCollision.cs
+++ EZ Smile Tooth Molds/Assets/Scripts/Collisions/SpoonCollision.cs
@@ -6,13 +6,20 @@ public class SpoonCollision : MonoBehaviour
 {
     public GameObject scoop;
     public GameObject myPrefab;
-   
-        void OnCollisionEnter(Collision collision)
+    Vector3 scoopPosition;
+    Vector3 ogPosition;
+
+    private void Start()
+    {
+        //ogPosition = scoop.transform.position;
+    }
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "alginate")
         {
+            scoopPosition = scoop.transform.position - new Vector3 (0,0,2);
             Destroy(scoop);
-            Instantiate(myPrefab, new Vector3(0.9f, -3.5f, 1.48f), Quaternion.identity);
+            Instantiate(myPrefab, scoopPosition, Quaternion.identity);
         }
     }
 }
